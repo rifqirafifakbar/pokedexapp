6565bd26dbfde4d5f01c8a9fadbb89adbc96d15e
Rifqi Rafif Akbar <rifqi.akbar@xtremax.com>
2022-08-02 08:58:17 +0700

    refactoring poke card for anchor parent

--- src/components/Organisms/Poke/PokeCard/PokeCard.js
+++ src/components/Organisms/Poke/PokeCard/PokeCard.js
@@ -13,41 +13,42 @@ export const PokeCard = ({ name, id, types, image, click }) => {
   }, [id]);
 
   return (
-    <div className="container-card mb-4">
-      <div>
-        <div className="text-center">
-          <h2 className="pokemon-name limit-text my-0">{name}</h2>
+    <Link to={click ? `/details/${name}` : "javascript:void"} className="card-poke">
+      <div className="container-card mb-4">
+        <div>
+          <div className="text-center">
+            <h2 className="pokemon-name limit-text my-0">{name}</h2>
+          </div>
+        </div>
+        <figure
+          className={`container-card-img position-relative my-4 container-${types[0].type.name}`}
+        >
+        
+            {error ? (
+              <img alt={name} title={name} src={pokemon_placeholder} />
+            ) : (
+              <img
+                onError={(e) => setError(true)}
+                alt={name}
+                title={name}
+                src={image}
+              />
+            )}
+          
+        </figure>
+        <div className="w-100  d-flex justify-content-between">
+          {types.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className={`${item.type.name} type-item ${types.length === 1 ? "w-100" : ''}`}
+              >
+                <p className="mb-0 text-uppercase">{item.type.name}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <figure
-        className={`container-card-img position-relative my-4 container-${types[0].type.name}`}
-      >
-        <Link to={click ? `/details/${name}` : "javascript:void"}>
-          {error ? (
-            <img alt={name} title={name} src={pokemon_placeholder} />
-          ) : (
-            <img
-              onError={(e) => setError(true)}
-              className="animation-up-down"
-              alt={name}
-              title={name}
-              src={image}
-            />
-          )}
-        </Link>
-      </figure>
-      <div className="w-100  d-flex justify-content-between">
-        {types.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className={`${item.type.name} type-item ${types.length === 1 ? "w-100" : ''}`}
-            >
-              <p className="mb-0 text-uppercase">{item.type.name}</p>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    </Link>
   );
 };
